950db1f6549cecb6688b79928337040680e3fe8b
Cyklone Hateka <emmanuelhateka1@gmail.com>
2024-01-16 22:03:19 +0000

    Changing entire login page design

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,23 @@
 import "../styles/pages/login.css";
-import { PiEyeSlash, PiEyeLight } from "react-icons/pi";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
   const [viewPassword, setViewPassword] = useState(false);
+  const [error, setError] = useState({
+    emailInputError: "",
+    passwordInputError: "",
+  });
+  const [resError, setResError] = useState("");
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: any) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
 
   const handlePasswordVisibility = () => {
     if (viewPassword) {
@@ -14,50 +27,82 @@ const Login = () => {
     }
   };
 
+  const navigate = useNavigate();
+
+  const { username, password } = input;
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (username.trim() === "") {
+      setError({ ...error, emailInputError: "emailInputError" });
+      return;
+    }
+    if (password.trim() === "") {
+      setError({ ...error, passwordInputError: "passwordInputError" });
+      return;
+    }
+  };
+
+  const handleNavigate = (e: any) => {
+    e.preventDefault();
+    navigate("/register");
+  };
+
   return (
     <div className="login">
       <div className="loginCont">
         <div className="imgCont">
           <img src="https://i.ibb.co/nMD7gp1/Bet-Love-Logo.png" alt="" />
         </div>
-        <div className="detailsCont">
-          <h2>login</h2>
-
-          <form action="">
-            <div className="email">
-              {/* the name is username because FastAPI Oauth two require username and password but actual input is email*/}
+        <h3>for the love of the game</h3>
+        <p>welcome back, please login to your account</p>
+        <form action="" onSubmit={handleSubmit}>
+          <div className="email">
+            {/* The field itself accepts an email but FastAPI Oauth2 requires a username and password */}
+            <label htmlFor="username">Email</label>
+            <input
+              type="email"
+              placeholder="example@mail.com"
+              id="username"
+              name="username"
+              className={error.emailInputError}
+              required
+              onChange={handleChange}
+            />
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <div className={error.passwordInputError}>
               <input
-                type="email"
-                name="username"
+                type={viewPassword ? "text" : "password"}
+                name="password"
+                id="password"
+                placeholder="Your Password"
                 required
-                placeholder="email"
+                onChange={handleChange}
               />
+              <p onClick={handlePasswordVisibility}>
+                {viewPassword ? <FaEyeSlash /> : <FaEye />}
+              </p>
             </div>
-            <div className="password">
-              <Link to="/">forgot password?</Link>
-              <div>
-                <input
-                  type={viewPassword ? "text" : "password"}
-                  name="password"
-                  required
-                  placeholder="password"
-                />
-                <p onClick={handlePasswordVisibility}>
-                  {viewPassword ? <PiEyeSlash /> : <PiEyeLight />}
-                </p>
-              </div>
-            </div>
+          </div>
+          <div className="rememberForgot">
             <div className="remember">
-              <input type="checkbox" name="remember" id="remember" />{" "}
-              <label htmlFor="">Remember me</label>
+              <input type="checkbox" name="rememberMe" id="rememberMe" />
+              <label htmlFor="rememberMe">Remember me</label>
             </div>
-            <button>login</button>
-
-            <p>
-              don't have an account yet? <Link to="/register">sign up</Link>
-            </p>
-          </form>
-        </div>
+            <Link to="/">Forgot password?</Link>
+          </div>
+          <div className="buttons">
+            <button className="loginButton" onClick={handleSubmit}>
+              Login
+            </button>
+            <button className="signUpButton" onClick={(e) => handleNavigate(e)}>
+              Sign Up
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
